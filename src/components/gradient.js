e5b1a93b212cb4b7f55a9feca1a4e65a265d57e8
Tanner Noble <tannernoble@gmail.com>
2018-10-05 13:59:31 -0500

    built gradient component

---
+++ src/components/gradient.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Gradient extends Component {
+  render () {
+    return (
+      <div className="gradient">
+         
+      </div>
+    );
+  }
+}

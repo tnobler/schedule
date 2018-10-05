55f019b0308a7570052ca51e6e269acfbadb8131
Tanner Noble <tannernoble@gmail.com>
2018-10-05 13:29:28 -0500

    app setup

---
+++ src/components/home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Course Scheduler</h1>
+        <h2>by Tnob</h2>
+      </div>
+    );
+  }
+}

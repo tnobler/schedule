cc45e16fbaee3d17365fc596c73871a5d3a9fc97
Tanner Noble <tannernoble@gmail.com>
2018-10-05 23:22:24 -0500

    added logic to toggle enrolled reducer

--- src/reducers/coursesReducer.js
+++ src/reducers/coursesReducer.js
@@ -11,8 +11,16 @@ export default function(state = [], action) {
         ...action.payload
       ]
     case TOGGLE_ENROLLED:
-      console.log(action.payload);
-      return state
+      console.log(action.payload)
+      const newCourses = state.map(course => {
+        if(course.id == action.payload) {
+          course.enrolled = !course.enrolled
+        }
+        return course
+      })
+      return [
+        ...newCourses
+      ]
     default:
       return state;
   }
